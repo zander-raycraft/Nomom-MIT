e803d969b93299bf9927517f275b43372c374f14
zander-raycraft <zanderraycraft504@gmail.com>
2023-12-04 14:56:48 -0600

    actual final commit

--- src/main.py
+++ src/main.py
@@ -1,14 +1,3 @@
-'''
-    1) Download this dataset and compute some
-        statistics to compare the performance of the two users. [DONE]
-
-    2) Did one of the users type faster? [DONE]
-
-    3) Was one user more efficient in the sense that they had to click fewer times to make a selection?
-
-    4) Explain your reasoning; your explanation should include some graphs of the data. [DONE]
-'''
-
 import pandas as pd
 import matplotlib.pyplot as mp
 
